7d9a8adf21381ba5af742869bf5519c8cfd8577c
Shyam-Limbasiya <shyamlimbasiya7@gmail.com>
2024-03-19 19:21:32 +0530

    read

--- springbootproj2/src/main/java/com/jpa/test/dao/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jpa.test.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import com.jpa.test.entities.user;
-
-public interface UserRepository extends CrudRepository<user,Integer> {
-
-}
